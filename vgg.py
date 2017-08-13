6f4ab69ef379c96915382113348338da7674f174
wondervictor <victorchanchina@gmail.com>
2017-08-13 17:32:05 +0800

    store parameters

--- vgg.py
+++ vgg.py
@@ -22,13 +22,15 @@ image = tf.placeholder(tf.float32, [None, image_size], name='image')
 label = tf.placeholder(tf.float32, [None, image_classes], name='label')
 
 
-def init_weights(shape):
-    return tf.Variable(tf.random_normal(shape, stddev=0.01))
+def init_weights(input_size, output_size):
+    return tf.Variable(tf.random_normal([input_size, output_size], stddev=0.01))
 
 
-def conv2d(name, x, filter_size, filter_nums, channels, b, parameters, stride=1):
-    conv_filter = init_weights([filter_size, filter_size, channels, filter_nums])
-    parameters['%s_conv_filter' % name] = conv_filter
+def init_filter(filter_size, filter_nums, channels):
+    return tf.Variable(tf.random_normal([filter_size, filter_size, channels, filter_nums]))
+
+
+def conv2d(name, x, conv_filter, b, stride=1):
     x = tf.nn.conv2d(x,
                      filter=conv_filter,
                      strides=[1, stride, stride, 1],
@@ -47,23 +49,15 @@ def maxpool(name, x, pool_size, stride):
 
 
 def init_bias(dim):
-    return tf.Variable(tf.random_normal(dim))
+    return tf.Variable(tf.random_normal([dim]))
 
 
-def fc(name, x, inputSize, outputSize, parameters):
-    weights = init_weights([inputSize, outputSize])
-    parameters['%s_weights' % name] = weights
-    bias = init_bias([outputSize])
-    parameters['%s_bias' % name] = bias
+def fc(name, x, weights, bias):
     fc1 = tf.matmul(x, weights)
     return tf.nn.relu(tf.add(fc1, bias))
 
 
-def softmax(name, x, inputSize, outputSize, parameters):
-    weights = init_weights([inputSize, outputSize])
-    parameters['%s_weights' % name] = weights
-    bias = init_bias([outputSize])
-    parameters['%s_bias' % name] = bias
+def softmax(name, x, weights, bias):
     fc1 = tf.matmul(x, weights)
     return tf.nn.softmax(tf.add(fc1, bias))
 
@@ -75,53 +69,87 @@ def dump_parameters(x):
 def vgg(x, parameters):
     x = tf.reshape(x, shape=[-1, 32, 32, 3])
     # 32
-    parameters['conv_1_bias'] = init_weights([64])
-    conv_1 = conv2d('conv_1', x, 3, 64, 3, parameters['conv_1_bias'], parameters)
-    parameters['conv_2_bias'] = init_weights([64])
-    conv_2 = conv2d('conv_2', conv_1, 3, 64, 64, parameters['conv_2_bias'], parameters)
+    conv_1 = conv2d('conv_1', x, parameters['conv_1_filter'], parameters['conv_1_bias'])
+    conv_2 = conv2d('conv_2', conv_1, parameters['conv_2_filter'], parameters['conv_2_bias'])
     pool_1 = maxpool('pool_1', conv_2, 2, 2)
     #   16 * 16 * 64
-    parameters['conv_3_bias'] = init_weights([128])
-    conv_3 = conv2d('conv_3', pool_1, 3, 128, 64, parameters['conv_3_bias'], parameters)
-    parameters['conv_4_bias'] = init_weights([128])
-    conv_4 = conv2d('conv_4', conv_3, 3, 128, 128, parameters['conv_4_bias'], parameters)
+    conv_3 = conv2d('conv_3', pool_1, parameters['conv_3_filter'], parameters['conv_3_bias'])
+    conv_4 = conv2d('conv_4', conv_3, parameters['conv_4_filter'], parameters['conv_4_bias'])
     pool_2 = maxpool('pool_2', conv_4, 2, 2)
     #   8 * 8 * 128
-    parameters['conv_5_bias'] = init_weights([256])
-    conv_5 = conv2d('conv_5', pool_2, 3, 256, 128, parameters['conv_5_bias'], parameters)
-    parameters['conv_6_bias'] = init_weights([256])
-    conv_6 = conv2d('conv_6', conv_5, 3, 256, 256, parameters['conv_6_bias'], parameters)
-    parameters['conv_7_bias'] = init_weights([256])
-    conv_7 = conv2d('conv_7', conv_6, 3, 256, 256, parameters['conv_7_bias'], parameters)
+    conv_5 = conv2d('conv_5', pool_2, parameters['conv_5_filter'], parameters['conv_5_bias'])
+    conv_6 = conv2d('conv_6', conv_5, parameters['conv_6_filter'], parameters['conv_6_bias'])
+    conv_7 = conv2d('conv_7', conv_6, parameters['conv_7_filter'], parameters['conv_7_bias'])
 
     pool_3 = maxpool('pool_3', conv_7, 2, 2)
     #   4 * 4 * 256
-    parameters['conv_8_bias'] = init_weights([512])
-    conv_8 = conv2d('conv_8', pool_3, 3, 512, 256, parameters['conv_8_bias'], parameters)
-    parameters['conv_9_bias'] = init_weights([512])
-    conv_9 = conv2d('conv_9', conv_8, 3, 512, 512, parameters['conv_9_bias'], parameters)
-    parameters['conv_10_bias'] = init_weights([512])
-    conv_10 = conv2d('conv_10', conv_9, 3, 512, 512, parameters['conv_10_bias'], parameters)
+    conv_8 = conv2d('conv_8', pool_3, parameters['conv_8_filter'], parameters['conv_8_bias'])
+    conv_9 = conv2d('conv_9', conv_8, parameters['conv_9_filter'], parameters['conv_9_bias'])
+    conv_10 = conv2d('conv_10', conv_9, parameters['conv_10_filter'], parameters['conv_10_bias'])
     pool_4 = maxpool('pool_4', conv_10, 2, 2)
     #   2 * 2 * 512
-    parameters['conv_11_bias'] = init_weights([512])
-    conv_11 = conv2d('conv_11', pool_4, 3, 512, 512, parameters['conv_11_bias'], parameters)
-    parameters['conv_12_bias'] = init_weights([512])
-    conv_12 = conv2d('conv_12', conv_11, 3, 512, 512, parameters['conv_12_bias'], parameters)
-    parameters['conv_13_bias'] = init_weights([512])
-    conv_13 = conv2d('conv_13', conv_12, 3, 512, 512, parameters['conv_13_bias'], parameters)
+    conv_11 = conv2d('conv_11', pool_4, parameters['conv_11_filter'], parameters['conv_11_bias'])
+    conv_12 = conv2d('conv_12', conv_11, parameters['conv_12_filter'], parameters['conv_12_bias'])
+    conv_13 = conv2d('conv_13', conv_12, parameters['conv_13_filter'], parameters['conv_13_bias'])
 
     pool_5 = maxpool('pool_5', conv_13, 2, 2)
     #   1 * 1 * 512
     pool_5 = tf.reshape(pool_5, [-1, 512])
     #   512
-    fc_1 = fc('fc_1', pool_5, 512, 4096, parameters)
-    fc_2 = fc('fc_2', fc_1, 4096, 4096, parameters)
-    fc_3 = fc('fc_3', fc_2, 4096, 1000, parameters)
-    output = softmax('softmax', fc_3, 1000, 100, parameters)
+    fc_1 = fc('fc_1', pool_5, parameters['fc_1_weights'], parameters['fc_1_bias'])
+    fc_2 = fc('fc_2', fc_1, parameters['fc_2_weights'], parameters['fc_2_bias'])
+    fc_3 = fc('fc_3', fc_2, parameters['fc_3_weights'], parameters['fc_3_bias'])
+    output = softmax('softmax', fc_3, parameters['softmax_weights'], parameters['softmax_bias'])
     return output
 
-parameters = dict()
+parameters = {
+    # stage 1
+    'conv_1_bias': init_bias(64),
+    'conv_1_filter': init_filter(3, 64, 3),
+    'conv_2_bias': init_bias(64),
+    'conv_2_filter': init_filter(3, 64, 64),
+    # stage 2
+    'conv_3_bias': init_bias(128),
+    'conv_3_filter': init_filter(3, 128, 64),
+    'conv_4_bias': init_bias(128),
+    'conv_4_filter': init_filter(3, 128, 128),
+    # stage 3
+    'conv_5_bias': init_bias(256),
+    'conv_5_filter': init_filter(3, 256, 128),
+    'conv_6_bias': init_bias(256),
+    'conv_6_filter': init_filter(3, 256, 256),
+    'conv_7_bias': init_bias(256),
+    'conv_7_filter': init_filter(3, 256, 256),
+    # stage 4
+    'conv_8_bias': init_bias(512),
+    'conv_8_filter': init_filter(3, 512, 256),
+    'conv_9_bias':init_bias(512),
+    'conv_9_filter': init_filter(3, 512, 512),
+    'conv_10_bias': init_bias(512),
+    'conv_10_filter': init_filter(3, 512, 512),
+    # stage 5
+    'conv_11_bias': init_bias(512),
+    'conv_11_filter': init_filter(3, 512, 512),
+    'conv_12_bias': init_bias(512),
+    'conv_12_filter': init_filter(3, 512, 512),
+    'conv_13_bias': init_bias(512),
+    'conv_13_filter': init_filter(3, 512, 512),
+
+    # fc weights and bias
+    'fc_1_weights': init_weights(512, 4096),
+    'fc_1_bias': init_bias(4096),
+
+    'fc_2_weights': init_weights(4096, 4096),
+    'fc_2_bias': init_bias(4096),
+
+    'fc_3_weights': init_weights(4096, 1000),
+    'fc_3_bias': init_bias(1000),
+
+    'softmax_weights': init_weights(1000, 100),
+    'softmax_bias': init_bias(100),
+
+}
+
 pred = vgg(image, parameters)
 
 # loss + optimize
@@ -157,6 +185,7 @@ with tf.Session() as sess:
                 acc_list.append(train_acc)
                 print("[Iter %s|Batch %s] LOSS=%.3f Train Accuracy=%.3f" % (step, i+1, loss, train_acc))
         batch_test_images, batch_test_labels = cifar.test_batch(128)
+
         print("[Testing Accuracy]: %.3f" % (sess.run(accuracy, feed_dict={image: batch_test_images, label: batch_test_labels})))
         step += 1
 
