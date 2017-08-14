66f83c58d30cc5e8b17d0683630c65f34db6af81
wondervictor <victorchanchina@gmail.com>
2017-08-14 15:44:27 +0800

    fix parameters's bug

--- vgg.py
+++ vgg.py
@@ -23,15 +23,15 @@ label = tf.placeholder(tf.float32, [None, image_classes], name='label')
 
 
 def init_weights(input_size, output_size):
-    return tf.Variable(tf.random_normal([input_size, output_size], stddev=0.01))
+    return tf.Variable(tf.random_normal([input_size, output_size], stddev=0.1))
 
 
 def init_filter(filter_size, filter_nums, channels):
-    return tf.Variable(tf.random_normal([filter_size, filter_size, channels, filter_nums]))
+    return tf.Variable(tf.random_normal([filter_size, filter_size, channels, filter_nums], stddev=0.1))
 
 
 def conv2d(name, x, conv_filter, b, stride=1):
-    x = tf.nn.conv2d(x,
+    x = tf.nn.conv2d(input=x,
                      filter=conv_filter,
                      strides=[1, stride, stride, 1],
                      padding='SAME',
@@ -41,7 +41,7 @@ def conv2d(name, x, conv_filter, b, stride=1):
 
 
 def maxpool(name, x, pool_size, stride):
-    return tf.nn.max_pool(x,
+    return tf.nn.max_pool(value=x,
                           ksize=[1, pool_size, pool_size, 1],
                           strides=[1, stride, stride, 1],
                           padding='SAME',
@@ -49,7 +49,7 @@ def maxpool(name, x, pool_size, stride):
 
 
 def init_bias(dim):
-    return tf.Variable(tf.random_normal([dim]))
+    return tf.Variable(tf.random_normal([dim], stddev=0.1))
 
 
 def fc(name, x, weights, bias):
@@ -66,41 +66,43 @@ def dump_parameters(x):
     print("[Shape]%s" % (x.shape))
 
 
-def vgg(x, parameters):
-    x = tf.reshape(x, shape=[-1, 32, 32, 3])
-    # 32
-    conv_1 = conv2d('conv_1', x, parameters['conv_1_filter'], parameters['conv_1_bias'])
-    conv_2 = conv2d('conv_2', conv_1, parameters['conv_2_filter'], parameters['conv_2_bias'])
-    pool_1 = maxpool('pool_1', conv_2, 2, 2)
-    #   16 * 16 * 64
-    conv_3 = conv2d('conv_3', pool_1, parameters['conv_3_filter'], parameters['conv_3_bias'])
-    conv_4 = conv2d('conv_4', conv_3, parameters['conv_4_filter'], parameters['conv_4_bias'])
-    pool_2 = maxpool('pool_2', conv_4, 2, 2)
-    #   8 * 8 * 128
-    conv_5 = conv2d('conv_5', pool_2, parameters['conv_5_filter'], parameters['conv_5_bias'])
-    conv_6 = conv2d('conv_6', conv_5, parameters['conv_6_filter'], parameters['conv_6_bias'])
-    conv_7 = conv2d('conv_7', conv_6, parameters['conv_7_filter'], parameters['conv_7_bias'])
-
-    pool_3 = maxpool('pool_3', conv_7, 2, 2)
-    #   4 * 4 * 256
-    conv_8 = conv2d('conv_8', pool_3, parameters['conv_8_filter'], parameters['conv_8_bias'])
-    conv_9 = conv2d('conv_9', conv_8, parameters['conv_9_filter'], parameters['conv_9_bias'])
-    conv_10 = conv2d('conv_10', conv_9, parameters['conv_10_filter'], parameters['conv_10_bias'])
-    pool_4 = maxpool('pool_4', conv_10, 2, 2)
-    #   2 * 2 * 512
-    conv_11 = conv2d('conv_11', pool_4, parameters['conv_11_filter'], parameters['conv_11_bias'])
-    conv_12 = conv2d('conv_12', conv_11, parameters['conv_12_filter'], parameters['conv_12_bias'])
-    conv_13 = conv2d('conv_13', conv_12, parameters['conv_13_filter'], parameters['conv_13_bias'])
-
-    pool_5 = maxpool('pool_5', conv_13, 2, 2)
-    #   1 * 1 * 512
-    pool_5 = tf.reshape(pool_5, [-1, 512])
-    #   512
-    fc_1 = fc('fc_1', pool_5, parameters['fc_1_weights'], parameters['fc_1_bias'])
-    fc_2 = fc('fc_2', fc_1, parameters['fc_2_weights'], parameters['fc_2_bias'])
-    fc_3 = fc('fc_3', fc_2, parameters['fc_3_weights'], parameters['fc_3_bias'])
-    output = softmax('softmax', fc_3, parameters['softmax_weights'], parameters['softmax_bias'])
-    return output
+# def vgg(x, parameters):
+#     x = tf.reshape(x, shape=[-1, 32, 32, 3])
+#     # 32
+#     conv_1 = conv2d('conv_1', x, parameters['conv_1_filter'], parameters['conv_1_bias'])
+#     conv_2 = conv2d('conv_2', conv_1, parameters['conv_2_filter'], parameters['conv_2_bias'])
+#     pool_1 = maxpool('pool_1', conv_2, 2, 2)
+#     #   16 * 16 * 64
+#     conv_3 = conv2d('conv_3', pool_1, parameters['conv_3_filter'], parameters['conv_3_bias'])
+#     conv_4 = conv2d('conv_4', conv_3, parameters['conv_4_filter'], parameters['conv_4_bias'])
+#     pool_2 = maxpool('pool_2', conv_4, 2, 2)
+#     #   8 * 8 * 128
+#     conv_5 = conv2d('conv_5', pool_2, parameters['conv_5_filter'], parameters['conv_5_bias'])
+#     conv_6 = conv2d('conv_6', conv_5, parameters['conv_6_filter'], parameters['conv_6_bias'])
+#     conv_7 = conv2d('conv_7', conv_6, parameters['conv_7_filter'], parameters['conv_7_bias'])
+#
+#     pool_3 = maxpool('pool_3', conv_7, 2, 2)
+#     #   4 * 4 * 256
+#     conv_8 = conv2d('conv_8', pool_3, parameters['conv_8_filter'], parameters['conv_8_bias'])
+#     conv_9 = conv2d('conv_9', conv_8, parameters['conv_9_filter'], parameters['conv_9_bias'])
+#     conv_10 = conv2d('conv_10', conv_9, parameters['conv_10_filter'], parameters['conv_10_bias'])
+#     pool_4 = maxpool('pool_4', conv_10, 2, 2)
+#     #   2 * 2 * 512
+#     conv_11 = conv2d('conv_11', pool_4, parameters['conv_11_filter'], parameters['conv_11_bias'])
+#     conv_12 = conv2d('conv_12', conv_11, parameters['conv_12_filter'], parameters['conv_12_bias'])
+#     conv_13 = conv2d('conv_13', conv_12, parameters['conv_13_filter'], parameters['conv_13_bias'])
+#
+#     pool_5 = maxpool('pool_5', conv_13, 2, 2)
+#     #   1 * 1 * 512
+#     pool_5 = tf.reshape(pool_5, [-1, 512])
+#     #   512
+#     fc_1 = fc('fc_1', pool_5, parameters['fc_1_weights'], parameters['fc_1_bias'])
+#     fc_2 = fc('fc_2', fc_1, parameters['fc_2_weights'], parameters['fc_2_bias'])
+#     fc_3 = fc('fc_3', fc_2, parameters['fc_3_weights'], parameters['fc_3_bias'])
+#     #output = tf.matmul(fc_3, parameters['softmax_weights'])
+#     #output = tf.add(output, parameters['softmax_bias'])
+#     output = softmax('softmax', fc_3, parameters['softmax_weights'], parameters['softmax_bias'])
+#     return output
 
 parameters = {
     # stage 1
@@ -150,22 +152,63 @@ parameters = {
 
 }
 
-pred = vgg(image, parameters)
+#pred = vgg(image, parameters)
+
+x = tf.reshape(image, shape=[-1, 32, 32, 3])
+# 32
+conv_1 = conv2d('conv_1', x, parameters['conv_1_filter'], parameters['conv_1_bias'])
+conv_2 = conv2d('conv_2', conv_1, parameters['conv_2_filter'], parameters['conv_2_bias'])
+pool_1 = maxpool('pool_1', conv_2, 2, 2)
+#   16 * 16 * 64
+conv_3 = conv2d('conv_3', pool_1, parameters['conv_3_filter'], parameters['conv_3_bias'])
+conv_4 = conv2d('conv_4', conv_3, parameters['conv_4_filter'], parameters['conv_4_bias'])
+pool_2 = maxpool('pool_2', conv_4, 2, 2)
+#   8 * 8 * 128
+conv_5 = conv2d('conv_5', pool_2, parameters['conv_5_filter'], parameters['conv_5_bias'])
+conv_6 = conv2d('conv_6', conv_5, parameters['conv_6_filter'], parameters['conv_6_bias'])
+conv_7 = conv2d('conv_7', conv_6, parameters['conv_7_filter'], parameters['conv_7_bias'])
+
+pool_3 = maxpool('pool_3', conv_7, 2, 2)
+#   4 * 4 * 256
+conv_8 = conv2d('conv_8', pool_3, parameters['conv_8_filter'], parameters['conv_8_bias'])
+conv_9 = conv2d('conv_9', conv_8, parameters['conv_9_filter'], parameters['conv_9_bias'])
+conv_10 = conv2d('conv_10', conv_9, parameters['conv_10_filter'], parameters['conv_10_bias'])
+pool_4 = maxpool('pool_4', conv_10, 2, 2)
+#   2 * 2 * 512
+conv_11 = conv2d('conv_11', pool_4, parameters['conv_11_filter'], parameters['conv_11_bias'])
+conv_12 = conv2d('conv_12', conv_11, parameters['conv_12_filter'], parameters['conv_12_bias'])
+conv_13 = conv2d('conv_13', conv_12, parameters['conv_13_filter'], parameters['conv_13_bias'])
+
+pool_5 = maxpool('pool_5', conv_13, 2, 2)
+#   1 * 1 * 512
+pool_5 = tf.reshape(pool_5, [-1, 512])
+#   512
+fc_1 = fc('fc_1', pool_5, parameters['fc_1_weights'], parameters['fc_1_bias'])
+fc_2 = fc('fc_2', fc_1, parameters['fc_2_weights'], parameters['fc_2_bias'])
+fc_3 = fc('fc_3', fc_2, parameters['fc_3_weights'], parameters['fc_3_bias'])
+output = tf.matmul(fc_3, parameters['softmax_weights'])
+output = tf.add(output, parameters['softmax_bias'])
+#pred_logits = tf.nn.softmax(tf.add(tf.matmul(fc_1, parameters['softmax_weights']), parameters['softmax_bias']))
+
+
+#softmax('softmax', fc_3, parameters['softmax_weights'], parameters['softmax_bias'])
 
 # loss + optimize
-cost = -tf.reduce_sum(label*tf.log(pred))
-optimizer = tf.train.RMSPropOptimizer(
-    learning_rate=learning_rate,
-    centered=True
-).minimize(cost)
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=output, labels=label))
+#cost = tf.reduce_sum(tf.pow(tf.subtract(pred_logits, label), 2))
+#tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=label, logits=pred_logits))
+adam_op = tf.train.AdamOptimizer(1e-3)
+optimizer = adam_op.minimize(cost)
 
-correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(label, 1))
+correct_pred = tf.equal(tf.argmax(output, 1), tf.argmax(label, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
 loss_list = []
 acc_list = []
 
-init = tf.initialize_all_variables()
+#grads_and_vars = adam_op.compute_gradients(cost)
+
+init = tf.global_variables_initializer()
 
 path = '/Users/vic/Dev/DeepLearning/Paddle/DeepLearningWithPaddle/GoogLeNet/data/'
 cifar = dataset.CIFAR(path + 'train', path + 'test')
@@ -173,17 +216,19 @@ cifar = dataset.CIFAR(path + 'train', path + 'test')
 with tf.Session() as sess:
     sess.run(init)
     step = 1
-
     while step <= training_iterations:
         for i in range(1+50000/batch_size):
             batch_images, batch_labels = cifar.next_batch(batch_size)
-            sess.run(optimizer, feed_dict={image: batch_images, label: batch_labels})
+            _, cost_= sess.run([optimizer, cost], feed_dict={image: batch_images, label: batch_labels})
+            #gra = sess.run(grads_and_vars, feed_dict={image: batch_images, label: batch_labels})
+            #print(gra)
+            print("COST: %s" % cost_)
             if i % 5 == 0:
                 train_acc = sess.run(accuracy, feed_dict={image: batch_images, label: batch_labels})
                 loss = sess.run(cost, feed_dict={image: batch_images, label: batch_labels})
                 loss_list.append(loss)
                 acc_list.append(train_acc)
-                print("[Iter %s|Batch %s] LOSS=%.3f Train Accuracy=%.3f" % (step, i+1, loss, train_acc))
+                print("[Iter %s|Batch %s] LOSS=%f Train Accuracy=%f" % (step, i+1, loss, train_acc))
         batch_test_images, batch_test_labels = cifar.test_batch(128)
 
         print("[Testing Accuracy]: %.3f" % (sess.run(accuracy, feed_dict={image: batch_test_images, label: batch_test_labels})))
